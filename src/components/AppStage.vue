018a17e4ba110f492373b17073f72d6b7690243b
Jason M Harrison <jason.m.harrison@gmail.com>
2020-12-01 22:52:46 -0600

    Initial project commit.

---
+++ src/components/AppStage.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+    <h1>App Stage</h1>
+
+    <chrome-picker v-model="colors" @input="updateColor" />
+
+    <section class="selected-color-display" :style="{ backgroundColor: `${selectedColor.hex}` }" />
+  </main>
+</template>
+
+<script>
+import { Chrome } from 'vue-color'
+import colorList from '../fixtures/colors'
+
+export default {
+  name: 'AppStage',
+  components: {
+    'chrome-picker': Chrome
+  },
+  data() {
+    return {
+      colors: colorList,
+      selectedColor: null
+    }
+  },
+  methods: {
+    updateColor(value) {
+      this.selectedColor = value;
+    }
+  }
+};
+</script>
+
+<style>
+.selected-color-display {
+  height: 20em;
+  width: 30em;
+}
+</style>
